e69bdfdd532d7cc49cf31fa01c70cab1be69156a
Fiorella <U202120418@upc.edu.pe>
2024-08-31 21:10:54 -0500

    feat(groups): Add GroupMemberController for managing group memberships

---
+++ src/main/java/b4u/pocketpartners/backend/groups/interfaces/rest/transform/CreateGroupMemberCommandFromResourceAssembler.java
@@ -0,0 +1,10 @@
+package b4u.pocketpartners.backend.groups.interfaces.rest.transform;
+
+import fairfinance.pocketpartners.backend.groups.domain.model.entities.GroupMember;
+import fairfinance.pocketpartners.backend.groups.interfaces.rest.resources.GroupMemberResource;
+
+public class CreateGroupMemberCommandFromResourceAssembler {
+    public static GroupMemberResource fromCommandToResource(GroupMember resource) {
+        return new GroupMemberResource(resource.getGroup().getId(), resource.getUserInformation().getId(), resource.getJoinedAt());
+    }
+}
